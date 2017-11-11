6b59c36303ae82bb9ff01241ddc4215604da3483
Philippe Marschall <philippe.marschall@gmail.com>
2017-11-11 19:33:30 +0100

    Make test run

--- src/test/java/com/github/marschall/aioj/capi/LibMemoryTest.java
+++ src/test/java/com/github/marschall/aioj/capi/LibMemoryTest.java
@@ -6,24 +6,25 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.nio.ByteBuffer;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
-public class LibMemoryTest {
+class LibMemoryTest {
 
-//  @Test
-  public void getPageSize() {
+  @Test
+  void getPageSize() {
     int pageSize = LibMemory.getPageSize();
     assertEquals(4096, pageSize);
   }
 
-//  @Test
-  public void allocateAligned() {
+  @Test
+  void allocateAligned() {
     ByteBuffer buffer = LibMemory.allocateAligned(512, 8192);
     assertNotNull(buffer);
     LibMemory.free(buffer);
   }
 
-//  @Test
-  public void getDirectBufferAddress() {
+  @Test
+  void getDirectBufferAddress() {
     ByteBuffer buffer = ByteBuffer.allocateDirect(512);
     long address = LibMemory.getDirectBufferAddress(buffer);
 
@@ -42,15 +43,15 @@ public class LibMemoryTest {
     assertEquals(256, LibMemory.getDirectBufferCapacity(slice));
   }
 
-//  @Test
+  @Test
   @Disabled("needs mmap()ed area")
-  public void madvise() {
+  void madvise() {
     ByteBuffer buffer = ByteBuffer.allocateDirect(512);
     assertEquals(0, LibMemory.madvise(buffer, MadviseArgument.MADV_SEQUENTIAL));
   }
 
-//  @Test
-  public void mlock() {
+  @Test
+  void mlock() {
     ByteBuffer buffer = ByteBuffer.allocateDirect(512);
     assertEquals(0, LibMemory.mlock(buffer));
     assertEquals(0, LibMemory.unmlock(buffer));
--- src/test/java/com/github/marschall/aioj/capi/PropertyTest.java
+++ src/test/java/com/github/marschall/aioj/capi/PropertyTest.java
@@ -1,6 +1,11 @@
 package com.github.marschall.aioj.capi;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +15,9 @@ class PropertyTest {
   void test() {
     String javaLibraryPath = System.getProperty("java.library.path");
     assertNotNull(javaLibraryPath);
-    System.err.println(javaLibraryPath);
+    Path path = Paths.get(javaLibraryPath);
+    assertTrue(Files.exists(path));
+    assertTrue(Files.isDirectory(path));
   }
 
 }
