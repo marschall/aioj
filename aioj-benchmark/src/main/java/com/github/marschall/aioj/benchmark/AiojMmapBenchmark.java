a27d9902c9eb3f0d3e16ff887746680dbad8d37e
Philippe Marschall <philippe.marschall@gmail.com>
2017-12-02 18:19:01 +0100

    bump custom mmap benchmark

--- aioj-benchmark/src/main/java/com/github/marschall/aioj/benchmark/AiojMmapBenchmark.java
+++ aioj-benchmark/src/main/java/com/github/marschall/aioj/benchmark/AiojMmapBenchmark.java
@@ -1,12 +1,54 @@
 package com.github.marschall.aioj.benchmark;
 
-public class AiojMmapBenchmark {
+import static com.github.marschall.aioj.benchmark.BufferUtils.sum;
+import static com.github.marschall.aioj.capi.MadviseArgument.MADV_DONTNEED;
+import static com.github.marschall.aioj.capi.MadviseArgument.MADV_FREE;
+import static com.github.marschall.aioj.capi.MadviseArgument.MADV_SEQUENTIAL;
+import static com.github.marschall.aioj.capi.MadviseArgument.MADV_WILLNEED;
+import static com.github.marschall.aioj.capi.OpenArgument.O_RDONLY;
 
-  private final boolean fadvise;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
-  public AiojMmapBenchmark(boolean fadvise) {
-    this.fadvise = fadvise;
-    // TODO Auto-generated constructor stub
+import com.github.marschall.aioj.capi.LibIo;
+import com.github.marschall.aioj.capi.LibMemory;
+import com.github.marschall.aioj.lowlevel.FileDescriptor;
+
+public class AiojMmapBenchmark implements FileBenchmark {
+
+  private final boolean madvise;
+
+  public AiojMmapBenchmark(boolean madvise) {
+    this.madvise = madvise;
+  }
+
+  @Override
+  public long read(String filename) throws IOException {
+    long sum = 0L;
+    try (FileDescriptor fileDescriptor = FileDescriptor.open(filename, O_RDONLY)) {
+      // FIXME
+      long size = 0L;
+      int increment = Integer.MAX_VALUE;
+      for (long position = 0; position < size; position += increment) {
+        int mapSize = Math.toIntExact(Math.min(size, position + increment) - position);
+        int prot = 0;
+        int flags = 0;
+        ByteBuffer buffer = fileDescriptor.mmap(mapSize, prot, flags, mapSize);
+
+        try {
+          if (this.madvise) {
+            LibMemory.madvise(buffer, MADV_WILLNEED | MADV_SEQUENTIAL);
+          }
+          sum  += sum(buffer, Math.toIntExact(mapSize));
+          if (this.madvise) {
+            LibMemory.madvise(buffer, MADV_DONTNEED | MADV_FREE);
+          }
+        } finally {
+          LibIo.munmap(buffer);
+        }
+      }
+    }
+    return sum;
   }
 
 }
