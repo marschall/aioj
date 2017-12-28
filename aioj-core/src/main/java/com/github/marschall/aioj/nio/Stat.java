cc772464e7e27f24c02067f93a3dec3424c28724
Philippe Marschall <philippe.marschall@gmail.com>
2017-12-28 21:52:04 +0100

    implement posix permissions

--- aioj-core/src/main/java/com/github/marschall/aioj/nio/Stat.java
+++ aioj-core/src/main/java/com/github/marschall/aioj/nio/Stat.java
@@ -6,8 +6,10 @@ import java.nio.file.attribute.PosixFileAttributes;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.UserPrincipal;
 import java.time.Instant;
+import java.util.EnumSet;
 import java.util.Set;
 
+import com.github.marschall.aioj.capi.OpenArgument;
 import com.github.marschall.aioj.capi.StructStat;
 
 public final class Stat {
@@ -92,8 +94,43 @@ public final class Stat {
 
     @Override
     public Set<PosixFilePermission> permissions() {
-      // TODO Auto-generated method stub
-      return null;
+      Set<PosixFilePermission> permissions = EnumSet.allOf(PosixFilePermission.class);
+      int mode = this.structStat.st_mode;
+
+      int ownerPermissions = mode & OpenArgument.S_IRWXU;
+      if ((ownerPermissions & OpenArgument.S_IRUSR) == 1) {
+        permissions.add(PosixFilePermission.OWNER_READ);
+      }
+      if ((ownerPermissions & OpenArgument.S_IWUSR) == 1) {
+        permissions.add(PosixFilePermission.OWNER_WRITE);
+      }
+      if ((ownerPermissions & OpenArgument.S_IXUSR) == 1) {
+        permissions.add(PosixFilePermission.OWNER_EXECUTE);
+      }
+
+      int groupPermissions = mode & OpenArgument.S_IRWXG;
+      if ((groupPermissions & OpenArgument.S_IRGRP) == 1) {
+        permissions.add(PosixFilePermission.GROUP_READ);
+      }
+      if ((groupPermissions & OpenArgument.S_IWGRP) == 1) {
+        permissions.add(PosixFilePermission.GROUP_WRITE);
+      }
+      if ((groupPermissions & OpenArgument.S_IXGRP) == 1) {
+        permissions.add(PosixFilePermission.GROUP_EXECUTE);
+      }
+
+      int otherPermissions = mode & OpenArgument.S_IRWXO;
+      if ((otherPermissions & OpenArgument.S_IROTH) == 1) {
+        permissions.add(PosixFilePermission.OTHERS_READ);
+      }
+      if ((otherPermissions & OpenArgument.S_IWOTH) == 1) {
+        permissions.add(PosixFilePermission.OTHERS_WRITE);
+      }
+      if ((otherPermissions & OpenArgument.S_IXOTH) == 1) {
+        permissions.add(PosixFilePermission.OTHERS_EXECUTE);
+      }
+
+      return permissions;
     }
 
   }
