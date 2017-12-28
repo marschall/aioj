6f9cdd9fb11bdad4f3b6a19995ecf4f625723751
Philippe Marschall <philippe.marschall@gmail.com>
2017-12-28 21:16:32 +0100

    Automatic .so unpacking

--- aioj-core/src/main/java/com/github/marschall/aioj/capi/LibraryLoader.java
+++ aioj-core/src/main/java/com/github/marschall/aioj/capi/LibraryLoader.java
@@ -1,22 +1,89 @@
 package com.github.marschall.aioj.capi;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 final class LibraryLoader {
 
   static final boolean INITIALIZED;
 
   static {
-    // -Djava.library.path=...
-    String javaLibraryPath = System.getProperty("java.library.path");
     boolean initialized = false;
+    String version = getVersion();
+    String libraryName = "aioj-core-" + version;
     try {
-      Runtime.getRuntime().loadLibrary("aioj-core-0.1.0-SNAPSHOT");
+      Runtime.getRuntime().loadLibrary(libraryName);
       initialized = true;
-      INITIALIZED = initialized;
-    } catch (RuntimeException | Error e) {
-      throw e;
+    } catch (UnsatisfiedLinkError e) {
+      // the library is not in the library path
+      // extract it from the JAR and load it from there
+      String fileName = System.mapLibraryName(libraryName);
+      Path extracted = extractLibrary(fileName);
+      try {
+        System.load(extracted.toAbsolutePath().toString());
+        initialized = true;
+      } finally {
+        // under Linux can be deleted after loading
+        try {
+          Files.delete(extracted);
+        } catch (IOException e1) {
+          throw new AssertionError("could not delete temp file: " + fileName, e);
+        }
+      }
     }
-//    String libraryName = System.mapLibraryName("libaioj-0.1.0-SNAPSHOT");
-//    System.load("/home/marschall/Documents/workspaces/default/aioj/target/nar/aioj-0.1.0-SNAPSHOT-amd64-Linux-gpp-jni/lib/amd64-Linux-gpp/jni/" + libraryName);
+    INITIALIZED = initialized;
+  }
+
+  private static String getVersion() {
+    String fileName = "aioj-core.version";
+    try (InputStream stream = LibraryLoader.class.getClassLoader().getResourceAsStream(fileName)) {
+      if (stream == null) {
+        throw new AssertionError("could not load resource: " + fileName);
+      }
+      ByteArrayOutputStream bos = new ByteArrayOutputStream(16);
+      byte[] buffer = new byte[16];
+      int read = 0;
+      do {
+        if (read > 0) {
+          bos.write(buffer, 0, read);
+        }
+        read = stream.read(buffer);
+      } while (read != -1);
+      return new String(bos.toByteArray(), StandardCharsets.US_ASCII);
+    } catch (IOException e) {
+      throw new AssertionError("could not load file: " + fileName, e);
+    }
+  }
+
+  private static Path extractLibrary(String fileName) {
+    Path tempFile;
+    try {
+      tempFile = Files.createTempFile("aioj-core-", ".so");
+    } catch (IOException e) {
+      throw new AssertionError("could not create temp file", e);
+    }
+    try (OutputStream output = Files.newOutputStream(tempFile);
+         InputStream input = LibraryLoader.class.getClassLoader().getResourceAsStream(fileName)) {
+      if (input == null) {
+        throw new AssertionError("could not load resource: " + fileName);
+      }
+      byte[] buffer = new byte[8192];
+      int read = 0;
+      do {
+        if (read > 0) {
+          output.write(buffer, 0, read);
+        }
+        read = input.read(buffer);
+      } while (read != -1);
+    } catch (IOException e) {
+      throw new AssertionError("could copy to temp file: " + fileName, e);
+    }
+    return tempFile;
   }
 
   static void assertInitialized() {
