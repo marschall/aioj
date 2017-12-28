d88039fad7d600cc57c2a532da26692fb46c01b7
Philippe Marschall <philippe.marschall@gmail.com>
2017-12-28 12:21:08 +0100

    Start with array access benchmark

--- aioj-benchmark/src/main/java/com/github/marschall/aioj/benchmark/ArrayAccessBenchmarks.java
+++ aioj-benchmark/src/main/java/com/github/marschall/aioj/benchmark/ArrayAccessBenchmarks.java
@@ -1,56 +1,85 @@
 package com.github.marschall.aioj.benchmark;
 
 import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
 
 /**
  * Benchmarks designed to measure the relative cost of accessing arrays
  * or different kinds of ByteBuffers.
  */
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Benchmark)
 public class ArrayAccessBenchmarks {
+
+
+
+  @Param("1024")
+  public int bufferSize;
 
   private ByteBuffer heapBuffer;
   private ByteBuffer directBuffer;
   private byte[] array;
 
-  private void setUp(int bufferSize) {
+  @Setup
+  public void setup() {
     this.array = new byte[bufferSize];
     this.heapBuffer = ByteBuffer.allocate(bufferSize);
     this.directBuffer = ByteBuffer.allocateDirect(bufferSize);
   }
 
-  public void array() {
+  @Benchmark
+  public void array(Blackhole blackhole) {
     for (byte b : array) {
-      // consume
+      blackhole.consume(b);
     }
   }
 
-  public void heapBufferAbsolute() {
+  @Benchmark
+  public void heapBufferAbsolute(Blackhole blackhole) {
     int capacity = this.heapBuffer.capacity();
     for (int i = 0; i < capacity; i++) {
-      this.heapBuffer.get(i);
+      byte b = this.heapBuffer.get(i);
+      blackhole.consume(b);
     }
   }
 
-  public void heapBufferRelative() {
+  @Benchmark
+  public void heapBufferRelative(Blackhole blackhole) {
     this.heapBuffer.position(0);
     int capacity = this.heapBuffer.capacity();
     for (int i = 0; i < capacity; i++) {
-      this.heapBuffer.get();
+      byte b = this.heapBuffer.get();
+      blackhole.consume(b);
     }
   }
 
-  public void directBufferAbsolute() {
+  @Benchmark
+  public void directBufferAbsolute(Blackhole blackhole) {
     int capacity = this.directBuffer.capacity();
     for (int i = 0; i < capacity; i++) {
-      this.heapBuffer.get(i);
+      byte b = this.heapBuffer.get(i);
+      blackhole.consume(b);
     }
   }
 
-  public void directBufferRelative() {
+  @Benchmark
+  public void directBufferRelative(Blackhole blackhole) {
     this.directBuffer.position(0);
     int capacity = this.directBuffer.capacity();
     for (int i = 0; i < capacity; i++) {
-      this.directBuffer.get();
+      byte b = this.directBuffer.get();
+      blackhole.consume(b);
     }
   }
 
---
+++ aioj-benchmark/src/main/java/com/github/marschall/aioj/benchmark/JmhMain.java
@@ -0,0 +1,31 @@
+package com.github.marschall.aioj.benchmark;
+
+import static org.openjdk.jmh.results.format.ResultFormatType.TEXT;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class JmhMain {
+
+  /**
+   * Main method, runs the benchmarks.
+   *
+   * @param args first element contains the file name
+   * @throws RunnerException  if something goes wrong during execution
+   */
+  public static void main(String[] args) throws RunnerException {
+    Options options = new OptionsBuilder()
+            .include("com.github.marschall.aioj.benchmark.*")
+            .warmupIterations(5)
+            .measurementIterations(5)
+            .forks(3)
+            .resultFormat(TEXT)
+            .threads(Integer.parseInt(args[0]))
+            .output(args[1])
+            .build();
+    new Runner(options).run();
+}
+
+}
